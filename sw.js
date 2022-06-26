c85d5ce2f1a5860abfc298f9c46eb9fc4ba04d59
Paweł Gargula <pawel.gargula@interia.pl>
2022-06-26 12:09:52 +0200

    Update cacheName

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // it will add newly resources
 // and remove old one in our Cache Storage
 // naming convension is: resources-yy-mm-dd_hh:mm
-const cacheName = 'resources-2022-06-26_12:03';
+const cacheName = 'resources-2022-06-26_12:09';
 
 const addResourcesToCache = async (resources) => {
     const cache = await caches.open(cacheName);
